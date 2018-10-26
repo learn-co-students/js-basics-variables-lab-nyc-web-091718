2346e3eeb3b86985d04b467e0cddd72523940c83
DDarrow123 <ddarrowphl@gmail.com>
2018-10-25 20:04:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+
+
+
+const companyName = 'Scuber';// Code your solution in this file!
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
